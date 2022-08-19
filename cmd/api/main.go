f84cca3815cada2a66f9043329e61d6c784e7dfa
mori-kei <mojavascript13@gmail.com>
2022-08-20 00:55:04 +0900

    handlerを追加

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const version = "1.0.0"
@@ -19,30 +20,31 @@ type AppStatus struct {
 	Environment string `json:"environment"`
 	Version string `json:"version"`
 }
+
+type application struct {
+	config config
+	logger *log.Logger
+}
 func main(){
 	var cfg config
 	flag.IntVar(&cfg.port,"port", 4000,"Server port to listen on")
 	flag.StringVar(&cfg.env,"env","development", "Application environment (develpment|production")
 	flag.Parse()
+	logger := log.New(os.Stdout,"", log.Ldate|log.Ltime)
+	app := &application{
+		config: cfg,
+		logger:logger,
+	}
 
-	fmt.Println("Running")
-
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request){
-		currentStatus := AppStatus{
-			Status: "Available",
-			Environment: cfg.env,
-			Version: version,
-		}
-		js,err := json.MarshalIndent(currentStatus, "", "\t")
-		if err != nil {
-			log.Print(err)
-		}
-		w.Header().Set("Content-Type","application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(js)
-	})
-
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port),nil)
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: app.routes(),
+		IdleTimeout:time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}	
+		logger.Println("Starting server on port", cfg.port)
+		err := srv.ListenAndServe()
 	if err != nil {
 		log.Println(err)
 	}
